94b83fde8450ea4fc1d60d0d41e99fd7e39cee3e
Anushadevi-S <anushadevis0804@gmail.com>
2026-05-16 18:49:15 +0530

    Add files via upload
    linear regration with multiple variable

---
+++ ML/1_linear_reg_multivariate/predictSalary.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn import linear_model
+from word2number import w2n
+import math
+
+df_hiringcsv = pd.read_csv("python/ML/1_linear_reg_multivariate/hiring.csv")
+print(df_hiringcsv)
+df_hiringcsv.experience = df_hiringcsv.experience.fillna('Zero')
+
+df_hiringcsv.experience = df_hiringcsv.experience.apply(w2n.word_to_num)
+score_mean= math.floor(df_hiringcsv['test_score(out of 10)'].mean())
+df_hiringcsv['test_score(out of 10)'] = df_hiringcsv['test_score(out of 10)'].fillna(score_mean)
+print(df_hiringcsv)
+
+model = linear_model.LinearRegression()
+model.fit(df_hiringcsv[['experience', 'test_score(out of 10)', 'interview_score(out of 10)']], df_hiringcsv['salary($)'])
+print(model.predict([[2, 9, 6]]))
+print(model.predict([[12, 10, 10]]))
